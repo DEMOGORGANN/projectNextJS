503f6bbfb5579503ea6cf4def5652f6eef15f70a
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2022-02-12 19:04:50 +0300

    settings document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import { AppProps } from "next/dist/shared/lib/router/router";
-import "../styles/globals.css";
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Создание NextJS приложения</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
