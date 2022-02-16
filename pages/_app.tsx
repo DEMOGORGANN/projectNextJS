a9ccdbd34a8aae274d9303619df8c561a92fa6e2
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2022-02-16 22:01:37 +0300

    app first components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
+import { Htag } from '../components';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
@@ -8,9 +10,13 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
             <Head>
                 <title>Создание NextJS приложения</title>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap" rel="stylesheet" />
             </Head>
 
             <Component {...pageProps} />
+            <Htag tag="h1">хуесос</Htag>
         </>
     );
 }
